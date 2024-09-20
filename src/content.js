a4a6caf27d63b338f9da711ac746f643e43fb1e8
Jose Dapena Paz <jdapena@igalia.com>
2024-09-20 10:12:34 +0200

    Show the URL of each document, both on registration and on logging events

--- src/content.js
+++ src/content.js
@@ -1,9 +1,11 @@
 const html = document.querySelector('html')
+const href = document.location.href
 const nativeObserver = new PerformanceObserver((list) => {
+  console.log("Container timing entries from " + href)
   console.log(list.getEntries());
 });
 nativeObserver.observe({ type: "container", buffered: true});
-console.debug("Registered observer")
+console.debug("Registered observer for " + href)
   
 html.setAttribute("containertiming", "")
 console.debug("Added containertiming attribute")
