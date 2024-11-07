e0564086017dff76508ef322a59e3c87d2e8a6d1
jwilliams720 <jwilliams720@bloomberg.net>
2024-11-07 17:44:14 +0000

    New changes
    - Remove overlays after 2 seconds
    - Add support for both SPAs and static sites

--- src/content.js
+++ src/content.js
@@ -2,6 +2,8 @@
 let flag = false;
 // Default selector
 let selector = 'html';
+// Container Timing element we've found
+let elm;
 
 // Pull the selector out of the settings
 chrome.storage.sync.get(
@@ -12,32 +14,47 @@ chrome.storage.sync.get(
 );
 
 const observer = new MutationObserver(() => {
-  if (document.querySelector(selector) && !flag) {
-    const html = document.querySelector(selector);
-    const href = document.location.href
-    const nativeObserver = new PerformanceObserver((list) => {
-      console.log("Container timing entries from " + href)
-      console.log(list.getEntries());
-      list.getEntries().forEach((list) => {
-        clearRects();
-        showRectsOnScreen(list.damagedRects);
-        showBoundingRect(list.intersectionRect);
-      })
-    });
-    nativeObserver.observe({ type: "container", buffered: true });
-    console.debug("Registered observer for " + href)
-
-    html.setAttribute("containertiming", "")
-    console.debug("Added containertiming attribute")
-    flag = true;
+  if ((elm = document.querySelector(selector)) && !flag) {
+    startPerformanceObserve(elm)
   }
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  observer.observe(document.body, { attributes: false, childList: true, characterData: false, subtree: true });
+  if (elm = document.querySelector(selector)) {
+    startPerformanceObserve(elm);
+    // We've found our element so we don't need to keep looking
+    // TODO: Change this so we can find multiple container timing elements (maybe by setting property on them)
+    flag = true
+  } else {
+    // TODO: At some point we will need this on regardless of whether we found our element or not (due to inner containers being injected)
+    observer.observe(document.body, { attributes: false, childList: true, characterData: false, subtree: true });
+  }
 });
 
+function startPerformanceObserve(elm) {
+  console.log(elm);
+  const href = document.location.href
+  const nativeObserver = new PerformanceObserver((list) => {
+    console.log("Container timing entries from " + href)
+    console.log(list.getEntries());
+    list.getEntries().forEach((list) => {
+      clearRects();
+      showRectsOnScreen(list.damagedRects);
+      showBoundingRect(list.intersectionRect);
+    })
+    // Now hide the rects so they're not in the way
+
+  });
+  nativeObserver.observe({ type: "container", buffered: true });
+  console.debug("Registered observer for " + href)
+
+  elm.setAttribute("containertiming", "")
+  console.debug("Added containertiming attribute")
+  flag = true;
+}
+
 function showRectsOnScreen(rects) {
+  // TODO We may want to batch these DOM updates
   rects.forEach((rect) => {
     const div = document.createElement('div');
     div.classList.add('overlay');
@@ -60,6 +77,21 @@ function showBoundingRect(rect) {
 }
 
 function clearRects() {
-  document.querySelectorAll('.overlay').forEach(elm => elm.remove());
-  document.querySelectorAll('.boundingRect').forEach(elm => elm.remove());
+  const fadeThenHide = (elm) => {
+    elm.animate(
+      [
+        { opacity: 1 },
+        { opacity: 0 },
+      ], {
+        duration: 1000
+      }
+    ).finished.then(() => {
+      elm.remove()
+    })
+  }
+
+  setTimeout(() => {
+    document.querySelectorAll('.overlay').forEach(elm => fadeThenHide(elm));
+    document.querySelectorAll('.boundingRect').forEach(elm => fadeThenHide(elm));
+  }, 2000)
 }
