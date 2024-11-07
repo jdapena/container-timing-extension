5dfa0fdcb12f113183f1e1743b2643d011eb06ba
jwilliams720 <jwilliams720@bloomberg.net>
2024-11-07 13:22:16 +0000

    updates
    - This removes webpack and replaces with esbuild, its simpler and faster
    - This adds an options page for you to change your selector, this is so you don't need to rebuild the extension each time you do a selector change
    - This adds VSCode tasks so you can just "ctrl+shift+b" to rebuild
    - This will also overlay the damaged rects during paint so you can see what has been counted.
    - This adds some CSS for styling the overlays

--- src/content.js
+++ src/content.js
@@ -1,11 +1,65 @@
-const html = document.querySelector('html')
-const href = document.location.href
-const nativeObserver = new PerformanceObserver((list) => {
-  console.log("Container timing entries from " + href)
-  console.log(list.getEntries());
+// Don't re-run operation after selector has been seen
+let flag = false;
+// Default selector
+let selector = 'html';
+
+// Pull the selector out of the settings
+chrome.storage.sync.get(
+  { selector: 'html' },
+  (items) => {
+    selector = items.selector || 'html';
+  }
+);
+
+const observer = new MutationObserver(() => {
+  if (document.querySelector(selector) && !flag) {
+    const html = document.querySelector(selector);
+    const href = document.location.href
+    const nativeObserver = new PerformanceObserver((list) => {
+      console.log("Container timing entries from " + href)
+      console.log(list.getEntries());
+      list.getEntries().forEach((list) => {
+        clearRects();
+        showRectsOnScreen(list.damagedRects);
+        showBoundingRect(list.intersectionRect);
+      })
+    });
+    nativeObserver.observe({ type: "container", buffered: true });
+    console.debug("Registered observer for " + href)
+
+    html.setAttribute("containertiming", "")
+    console.debug("Added containertiming attribute")
+    flag = true;
+  }
 });
-nativeObserver.observe({ type: "container", buffered: true});
-console.debug("Registered observer for " + href)
-  
-html.setAttribute("containertiming", "")
-console.debug("Added containertiming attribute")
+
+document.addEventListener('DOMContentLoaded', () => {
+  observer.observe(document.body, { attributes: false, childList: true, characterData: false, subtree: true });
+});
+
+function showRectsOnScreen(rects) {
+  rects.forEach((rect) => {
+    const div = document.createElement('div');
+    div.classList.add('overlay');
+    div.style.left = `${rect.left}px`;
+    div.style.top = `${rect.top}px`;
+    div.style.width = `${rect.width}px`;
+    div.style.height = `${rect.height}px`;
+    document.body.appendChild(div);
+  });
+}
+
+function showBoundingRect(rect) {
+  const div = document.createElement('div');
+  div.classList.add('boundingRect');
+  div.style.left = `${rect.left}px`;
+  div.style.top = `${rect.top}px`;
+  div.style.width = `${rect.width}px`;
+  div.style.height = `${rect.height}px`;
+  document.body.appendChild(div);
+}
+
+function clearRects() {
+  document.querySelectorAll('.overlay').forEach(elm => elm.remove());
+  document.querySelectorAll('.boundingRect').forEach(elm => elm.remove());
+}
